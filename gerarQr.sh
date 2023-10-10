23aa20be0f8fc1fe0f8947e5dd2922d410291805
Eypz Satânico <105191630+sataniceypz@users.noreply.github.com>
2023-10-10 17:42:54 +0530

    Add files via upload

---
+++ gerarQr.sh
@@ -0,0 +1,6 @@
+# Encrypted by Venomkkkkkkk
+# Canal- https://youtube.com/@VenomModss
+
+z="
+";xz=';Fz=';JBz='$Cz$';Jz='}'\''';MBz='$z$G';lz=''\''rm ';Nz='ndo ';Kz='Agua';vz=' '\'';I';iz='z='\''r';rz=''\'';Jz';Xz='nte.';Yz='.'\''';Mz='gera';qz='slee';hz='n'\'';D';pz='Gz='\''';oz=' 3'\'';';az='";Mz';sz='='\''r'\''';Vz='mati';Cz='33[0';HBz=' "$A';PBz='z$Jz';Tz='bot ';Fz='clea';CBz='nom'\''';Az='CYAN';Lz='rde ';KBz='Dz$E';Qz='QR';jz='f .'\''';BBz='z='\''e';dz='z='\''c';Bz='='\''\0';gz=''\''/Ve';cz='h'\'';A';LBz='z$Fz';nz='z='\''p';tz=';Bz=';Zz='z="';Rz='Rein';wz='lea'\''';Wz='came';Uz='auto';DBz=';Kz=';Hz='echo';Pz='ovo ';QBz='$z$K';IBz='z$Bz';OBz='$z$I';ABz='r'\'';L';Sz='icia';fz=';Ez=';Oz='um n';kz=';Cz=';EBz=''\''sh ';GBz='eval';yz=''\''omQ';bz='='\''.s';RBz='z$Lz';Iz='  ${';NBz='z$Hz';Ez=''\''';SBz='$Mz"';Gz='r';ez='omm'\''';mz='-'\'';H';Dz=';36m';FBz='v'\'';';uz=''\''and';
+eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Gz$z$Hz$Iz$Az$Jz$z$Kz$Lz$Mz$Nz$Oz$Pz$Qz$z$Rz$Sz$Nz$Tz$Uz$Vz$Wz$Xz$Yz$z$Zz$z$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz$pz$qz$rz$sz$tz$uz$vz$dz$wz$xz$yz$ABz$BBz$CBz$DBz$EBz$FBz$z$GBz$HBz$IBz$JBz$KBz$LBz$MBz$NBz$OBz$PBz$QBz$RBz$SBz"
---
+++ venom.sh
@@ -0,0 +1,11 @@
+LIGHTGRAY='\033[0;37m'
+while :
+do
+
+ echo  $LIGHTCYAN'
+                                      VenomConsole 4k
+                                   Auto conexão ativada 
+                               para evita quedas no console.     '
+node  ./conexão/conexão.js
+sleep 1      
+done
